f8b5f214a37d43f0fc10218dacca31c09bbc73e5
Jorge Lopez <110996480+Jorge162017@users.noreply.github.com>
2023-11-22 01:14:14 -0600

    vista Teams

--- app/src/main/java/com/example/laboratorio7/tournaments/itemTournament/MatchTournament.kt
+++ app/src/main/java/com/example/laboratorio7/tournaments/itemTournament/MatchTournament.kt
@@ -1,5 +1,6 @@
 package com.example.laboratorio7.tournaments.itemTournament
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,15 +9,21 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -27,12 +34,12 @@ fun MatchTournament(tournament:String){
         modifier = Modifier.fillMaxSize(),
                 color = Color.Black
     ) {
-
-
         Column(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.verticalScroll(rememberScrollState())
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(bottom = 50.dp)
         ) {
             Surface(
                 modifier = Modifier
@@ -62,15 +69,149 @@ fun MatchTournament(tournament:String){
                 horizontalAlignment = Alignment.Start,
                 verticalArrangement = Arrangement.Center
             ) {
-                Text(
-                    text = "PARTIDOS",
-                    modifier = Modifier.padding(start = 20.dp),
-                    fontSize = 25.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                )
-                Spacer(modifier = Modifier.height(50.dp))
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(Color.White)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        // Texto en la parte superior de la tarjeta
+                        Text(
+                            text = "PARTIDO 1",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Text(
+                            text = "Resultado 3 - 0",
+                        )
+                        Text(
+                            text = "Equipo 1",
+                        )
+                        Text(
+                            text = "contra",
+                        )
+                        Text(
+                            text = "equipo 6",
+                        )
+
+                        // Espaciador vertical
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        // Botón debajo del texto
+                        Button(
+                            onClick = {
+                                // Acción a realizar cuando se hace clic en el botón
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
+                            Text(text = "Cambiar Resultado")
+                        }
+                    }
+                }
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(Color.White)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        // Texto en la parte superior de la tarjeta
+                        Text(
+                            text = "PARTIDO 2",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Text(
+                            text = "Resultado 1 - 2",
+                        )
+                        Text(
+                            text = "Equipo 3",
+                        )
+                        Text(
+                            text = "contra",
+                        )
+                        Text(
+                            text = "equipo 5",
+                        )
+
+                        // Espaciador vertical
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        // Botón debajo del texto
+                        Button(
+                            onClick = {
+                                // Acción a realizar cuando se hace clic en el botón
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
+                            Text(text = "Cambiar Resultado")
+                        }
+                    }
+                }
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(Color.White)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        // Texto en la parte superior de la tarjeta
+                        Text(
+                            text = "PARTIDO 3",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp
+                        )
+                        Spacer(modifier = Modifier.height(16.dp))
+                        Text(
+                            text = "Resultado 1 - 1",
+                        )
+                        Text(
+                            text = "Equipo 2",
+                        )
+                        Text(
+                            text = "contra",
+                        )
+                        Text(
+                            text = "equipo 4",
+                        )
+
+                        // Espaciador vertical
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        // Botón debajo del texto
+                        Button(
+                            onClick = {
+                                // Acción a realizar cuando se hace clic en el botón
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
+                            Text(text = "Cambiar Resultado")
+                        }
+                    }
+                }
             }
         }
     }
+
 }
--- app/src/main/java/com/example/laboratorio7/tournaments/itemTournament/TeamsTournament.kt
+++ app/src/main/java/com/example/laboratorio7/tournaments/itemTournament/TeamsTournament.kt
@@ -1,5 +1,7 @@
 package com.example.laboratorio7.tournaments.itemTournament
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -8,17 +10,24 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.rememberImagePainter
+import com.example.laboratorio7.R
 
 
 @Composable
@@ -30,7 +39,9 @@ fun TeamsTournament(tournament:String){
         Column(
             verticalArrangement = Arrangement.Top,
             horizontalAlignment = Alignment.CenterHorizontally,
-            modifier = Modifier.verticalScroll(rememberScrollState())
+            modifier = Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(bottom = 50.dp)
         ) {
             Surface(
                 modifier = Modifier
@@ -60,14 +71,114 @@ fun TeamsTournament(tournament:String){
                 horizontalAlignment = Alignment.Start,
                 verticalArrangement = Arrangement.Center
             ) {
-                Text(
-                    text = "EQUIPOS DISPONIBLES",
-                    modifier = Modifier.padding(start = 20.dp),
-                    fontSize = 25.sp,
-                    color = Color.White,
-                    fontWeight = FontWeight.Bold,
-                )
-                Spacer(modifier = Modifier.height(50.dp))
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(Color.White)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.prueba),
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        // Texto en la parte superior de la tarjeta
+                        Text(
+                            text = "EQUIPO 1",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp
+                        )
+                        Text(
+                            text = "Partidos Jugados(PJ): 3",
+                        )
+                        Text(
+                            text = "Partidos Ganados(PG): 2",
+                            color = Color(56, 176, 0)
+                        )
+                        Text(
+                            text = "Partidos Empatados(PE): 1",
+                            color = Color(250, 163, 7)
+                        )
+                        Text(
+                            text = "Partidos Perdidos(PP): 0",
+                            color = Color(208, 0, 0)
+                        )
+
+                        // Espaciador vertical
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        // Botón debajo del texto
+                        Button(
+                            onClick = {
+                                // Acción a realizar cuando se hace clic en el botón
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
+                            Text(text = "Ver Plantilla")
+                        }
+                    }
+                }
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp)
+                        .clip(RoundedCornerShape(8.dp))
+                        .background(Color.White)
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp),
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.prueba2),
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                        // Texto en la parte superior de la tarjeta
+                        Text(
+                            text = "EQUIPO 2",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 20.sp
+                        )
+                        Text(
+                            text = "Partidos Jugados(PJ): 3",
+                        )
+                        Text(
+                            text = "Partidos Ganados(PG): 1",
+                            color = Color(56, 176, 0)
+                        )
+                        Text(
+                            text = "Partidos Empatados(PE): 1",
+                            color = Color(250, 163, 7)
+                        )
+                        Text(
+                            text = "Partidos Perdidos(PP): 1",
+                            color = Color(208, 0, 0)
+                        )
+
+                        // Espaciador vertical
+                        Spacer(modifier = Modifier.height(16.dp))
+
+                        // Botón debajo del texto
+                        Button(
+                            onClick = {
+                                // Acción a realizar cuando se hace clic en el botón
+                            },
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ) {
+                            Text(text = "Ver Plantilla")
+                        }
+                    }
+                }
             }
         }
     }
